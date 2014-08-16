2f3a7f72d44fc76cd7666bb2cd333b70aca370bc
Alexoner <onerhao@gmail.com>
2014-08-16 21:38:21 +0800

    use global variable correctly

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ urlfetch.set_default_fetch_deadline(60)
 count = -1
 userId = -1
 deviceId = -1
-time_key =
+# global time_key
 
 
-#CUR_TIME = strftime("%a, %d %b %H:%M:%S", gmtime())
+# CUR_TIME = strftime("%a, %d %b %H:%M:%S", gmtime())
 
 
 MAIN_PAGE_HTML = """\
@@ -25,7 +25,7 @@ MAIN_PAGE_HTML = """\
     <form action="/parking/countdown" method="post">
         <button>countdown</button>
     </form>
-	
+
     <form action="/parking/getcount" method="post">
         count:<input type="text" name="count"></br>
         deviceId:<input type="text" name="deviceId"></br>
@@ -35,64 +35,76 @@ MAIN_PAGE_HTML = """\
     <form action="/parkingservice/history" method="post">
         <button>gettime</button>
     </form>
-	
+
     <form action=""
   </body>
 </html>
 """
+
+
 class History(ndb.Model):
 
     time = ndb.DateTimeProperty(auto_now_add=True)
 history = History()
-    
+time_key = None
+
+
 class MainPage(webapp2.RequestHandler):
+
     def get(self):
-        #f=open('workfile.txt','r')
+        # f=open('workfile.txt','r')
         self.response.write(MAIN_PAGE_HTML)
-        #self.response.write(f.read())
+        # self.response.write(f.read())
         self.response.write('<html><body>')
 
+
 class Countdown(webapp2.RequestHandler):
+
     def get(self):
         self.response.write(count)
         pass
+
     def post(self):
         self.response.write(count)
-        #self.response.write(history.time)
+        # self.response.write(history.time)
         pass
+
 
 class Gettime(webapp2.RequestHandler):
+
     def get(self):
+        global time_key
         deadline = time_key.get()
         self.response.write(deadline)
+
     def post(self):
+        global time_key
         deadline = time_key.get()
         self.response.write(deadline)
-        
-        
+
+
 class Getcount(webapp2.RequestHandler):
+
     def get(self):
         post(self)
-
 
     def post(self):
         global count
         print "here ha ha ha "
-        f = open('UserId.txt','r')
+        f = open('UserId.txt', 'r')
         userId = f.read()
         self.response.write(userId)
-
 
         count = self.request.get('count')
         if count == '':
             count = -1
-           
+
         deviceId = self.request.get('deviceId')
         if deviceId == '':
-            deviceId =-1
+            deviceId = -1
         self.response.write(deviceId)
 
-        if count < 0 or deviceId <0:
+        if count < 0 or deviceId < 0:
             self.response.write("error1")
             return
         elif userId != deviceId:
@@ -100,33 +112,29 @@ class Getcount(webapp2.RequestHandler):
             return
         else:
             self.response.write("success")
-            #self.response.write(count)
+            # self.response.write(count)
             if count == '0':
-                #self.response.write('count:'+count)
+                # self.response.write('count:'+count)
                 history = History()
                 tt = time.time()
                 tt += time.altzone
-                #self.response.write(tt)
+                # self.response.write(tt)
                 tt += 9 * 3600
-                #self.response.write(tt)
+                # self.response.write(tt)
                 history.time = datetime.datetime.utcfromtimestamp(tt)
                 self.response.write(history.time)
                 self.response.write('%%%%%%%1')
+                global time_key
                 time_key = history.put()
                 self.response.write('%%%%%%%2')
-
-
-
-
-
 
 
 routes = [
     ('/', MainPage),
     ('/parking/countdown', Countdown),
     #('/parking/countdown/gfgADSFDF',ParkingSlot),
-    ('/parking/getcount',Getcount),
-    ('/parkingservice/history',Gettime),
+    ('/parking/getcount', Getcount),
+    ('/parkingservice/history', Gettime),
 ]
 
 application = webapp2.WSGIApplication(routes=routes, debug=True)
